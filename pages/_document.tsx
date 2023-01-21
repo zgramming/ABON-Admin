7ddfb53ca8767277e377d08380abc96182440930
Zeffry Reynando <zeffry.reynando@gmail.com>
2023-01-21 14:10:05 +0700

    Init Default Style Color & Fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,28 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;600;700;800;900&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Hind+Vadodara:wght@300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body className="font-hind-vadodara">
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,23 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        rubik: ["Rubik", "sans-serif"],
+        "hind-vadodara": ["Hind Vadodara", "sans-serif"],
+      },
+      colors: {
+        primary: "#63A46C",
+        accent: "#7994DA",
+        success: "#00D8AC",
+        info: "#5CB8E4",
+        warning: "#ffb200",
+        error: "#FF4A4A",
+      },
+    },
   },
   plugins: [],
-}
+  corePlugins: {
+    preflight: false,
+  },
+};
